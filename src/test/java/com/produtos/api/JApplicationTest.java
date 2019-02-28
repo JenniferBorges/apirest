73b3deb644496e2cda3045007d053271524f317d
jennifer.oliveira <jennifer.oliveira@leucotron.com.br>
2019-02-28 16:25:27 -0300

    criando a primeira entidade e estabelecendo conexão com banco de dados postgres

---
+++ src/test/java/com/produtos/api/JApplicationTest.java
@@ -0,0 +1,21 @@
+package com.produtos.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ *
+ * @author jennifer
+ *
+ */
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+public class JApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
